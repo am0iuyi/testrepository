f47b32b448ba0efbc4e3fe4d25275cebc9903609
Alina376 <AlinaK73@yandex.ru>
2025-04-08 18:06:20 +0400

    registration

--- main/main/urls.py
+++ main/main/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.main_page,),
     path('admin/', admin.site.urls),
 ]
--- main/main/views.py
+++ main/main/views.py
@@ -5,19 +5,7 @@ from django.shortcuts import render
 from .models import Game
 
 
-# def html(request):
-#     return HttpResponse(
-#         '''<h1> алфавит </h1>
-#         <a href = 'https://avatars.mds.yandex.net/i?id=6e6a84f5253b938007c377ccd2d708e31a02c24a-10231558-images-thumbs&n=13'> алфавит </a>
-#         <br></br>
-#         <img src='https://cdn1.ozone.ru/multimedia/1018056524.jpg' wide='100' height='200' >
-#         <h2> Ру́сский алфави́т (ру́сская а́збука) — алфавит русского языка, включающий 33 буквы.</h2>
-#         <p> Алфавит создали в 863году, братья кирилл и мефодий, по указц византийского императора
-#         <h3> состав алфавита</h3>
-#         </p> a б в г д е ё ж з и й к л м н о п р с т у ф х ц ч ш щ ъ ы ь э ю я </p>
-#         <h1> всем спасибо за внимание <h1>
-#         '''
-#     )
+
 def index(request: HttpRequest):
     result=0
 
@@ -31,6 +19,5 @@ def index(request: HttpRequest):
     return render(request, "index.html",{"result":result,})
 
 
-def test(request: HttpRequest):
-    print(*Game.objects.all())
-    return ""
+def main_page(request: HttpRequest):
+    return render(request,'main_page.html')
